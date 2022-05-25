c12b7be2fc701b2007fa5100877239d5403fd2fa
Serge.Povalyaev <Serge.Povalyaev@x5.ru>
2022-05-25 23:37:57 +0300

    HW07 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,86 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrFileNotFound          = errors.New("file not found")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	var sourceFile *os.File
+	sourceFile, err := os.Open(fromPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return ErrFileNotFound
+		}
+		return err
+	}
+	defer sourceFile.Close()
+
+	fileInfo, err := sourceFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	fileSize := fileInfo.Size()
+	if fileSize == 0 || fileInfo.IsDir() {
+		return ErrUnsupportedFile
+	}
+
+	if offset > fileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	targetFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer targetFile.Close()
+
+	if limit == 0 {
+		limit = fileSize - offset
+	}
+
+	n := offset + limit
+
+	bar := pb.StartNew(getBarSize(fileSize, offset, limit))
+
+	buf := make([]byte, 1)
+	for offset < n {
+		_, err := sourceFile.ReadAt(buf, offset)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+
+		_, err = targetFile.Write(buf)
+		if err != nil {
+			return err
+		}
+
+		bar.Increment()
+
+		offset++
+	}
+
+	bar.Finish()
+
 	return nil
+}
+
+func getBarSize(fileSize, offset, limit int64) int {
+	if fileSize > offset+limit {
+		return int(limit)
+	}
+
+	return int(fileSize - offset)
 }
