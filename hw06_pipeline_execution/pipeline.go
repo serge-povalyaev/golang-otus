30da4a7b29c16d3a5353630a277697e19feaf5a6
Serge.Povalyaev <Serge.Povalyaev@x5.ru>
2022-05-30 10:59:08 +0300

    HW06 - fixes

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,26 +9,30 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	resultChan := in
+	resultChan := stageDone(done, in)
 	for _, stage := range stages {
-		stageChan := make(Bi)
-		go func(inChan In, outChan Bi) {
-			defer close(outChan)
-			for {
-				select {
-				case <-done:
-					return
-				case result, ok := <-inChan:
-					if !ok {
-						return
-					}
-					outChan <- result
-				}
-			}
-		}(resultChan, stageChan)
-
-		resultChan = stage(stageChan)
+		resultChan = stageDone(done, stage(resultChan))
 	}
 
 	return resultChan
+}
+
+func stageDone(done In, resultChan In) Out {
+	stageChan := make(Bi)
+	go func(inChan In, outChan Bi) {
+		defer close(outChan)
+		for {
+			select {
+			case <-done:
+				return
+			case result, ok := <-inChan:
+				if !ok {
+					return
+				}
+				outChan <- result
+			}
+		}
+	}(resultChan, stageChan)
+
+	return stageChan
 }
